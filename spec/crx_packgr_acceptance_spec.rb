c6a705d0b33b5aaed273749e8e2a02c70a1cdb70
Bryan Stopp <bstopp@github.com>
2016-11-29 08:11:03 -0600

    All CRX Package manager API end points.

--- spec/crx_packgr_acceptance_spec.rb
+++ spec/crx_packgr_acceptance_spec.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/env ruby
 # Load the gem
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'client', 'crx-packmgr', 'lib'))
+
 require 'crx_packmgr_api_client'
 require 'json'
+require 'xmlsimple'
 
 config = CrxPackageManager::Configuration.new
 config.debugging = false
 config.username = 'admin'
 config.password = 'admin'
+#config.debugging = true
 #config.host= 'localhost:4503'
 
 api_instance = CrxPackageManager::DefaultApi.new
@@ -15,7 +19,108 @@ api_instance.api_client.config = config
 begin
   #List all available packages.
   result = api_instance.list(:path => '/etc/packages/Adobe/granite/com.adobe.granite.httpcache.content-1.0.2.zip')
-  p result.results[0]
+  p "Package List: #{result.results[0]}"
 rescue CrxPackageManager::ApiError => e
   puts "Exception when calling DefaultApi->list: #{e}"
+end
+
+
+begin
+  # Download a package
+  result = api_instance.download(:path => '/etc/packages/Adobe/granite/com.adobe.granite.httpcache.content-1.0.2.zip')
+  p "Download: #{result}"
+rescue CrxPackageManager::ApiError => e
+  puts "Exception when calling DefaultApi->list: #{e}"
+end
+
+begin
+  # List all Groups
+  result = api_instance.groups
+  p "Group: #{result.groups[0]}"
+rescue CrxPackageManager::ApiError => e
+  puts "Exception when calling DefaultApi->groups: #{e}"
+end
+
+begin
+  # Display metadata
+  result = api_instance.init
+  p "Metadata: #{result}"
+rescue CrxPackageManager::ApiError => e
+  puts "Exception when calling DefaultApi->init: #{e}"
+end
+
+begin
+  # Display installstatus
+  result = api_instance.installstatus
+  p "Install Status: #{result}"
+rescue CrxPackageManager::ApiError => e
+  puts "Exception when calling DefaultApi->installstatus: #{e}"
+end
+
+begin
+  # Get a Screenshot
+  result = api_instance.screenshot(path: '/etc/packages/RedRobin/all-content.zip/jcr:content/vlt:definition/screenshots/462bcbba-057a-4171-9228-1a798646b2a9')
+  p "Screenshot: #{result}"
+rescue CrxPackageManager::ApiError => e
+  puts "Exception when calling DefaultApi->screenshot: #{e}"
+end
+
+
+begin
+  # Get a Thumbnail
+  result = api_instance.thumbnail(path: '/etc/packages/adobe/consulting/acs-aem-commons-content-3.4.0.zip')
+  p "Thumbnail: #{result}"
+rescue CrxPackageManager::ApiError => e
+  puts "Exception when calling DefaultApi->thumbnail: #{e}"
+end
+
+begin
+  # Update a package
+  ssc = [{
+      'upload': true
+  }].to_json.to_s
+  ss = File.new('/Users/stopp/Downloads/aem-local.jpeg')
+  result = api_instance.update(
+      '/etc/packages/my_packages/test-1.zip',
+      'test',
+      group_name: 'my_packages',
+      version: 1,
+      screenshot: ss,
+      screenshot_config: ssc
+  )
+  data = JSON.parse(result).to_hash
+  status = CrxPackageManager::UpdateStatus.new(data)
+  p "Package update: #{status}"
+rescue CrxPackageManager::ApiError => e
+  puts "Exception when calling DefaultApi->update: #{e}"
+end
+
+begin
+  # Service list help.
+  result = api_instance.service_get('help', name: 'test1')
+  puts result
+  hash = XmlSimple.xml_in(result, ForceArray: false, KeyToSymbol: true)
+  response = CrxPackageManager::ServiceResponse.new
+  data = response.build_from_hash(hash)
+  p "Service Response: #{data.response}"
+rescue CrxPackageManager::ApiError => e
+  puts "Exception when calling DefaultApi->service: #{e}"
+end
+
+begin
+  # Upload a package
+  package = File.new('/Users/stopp/Downloads/acs-aem-commons-content-3.4.0-min.zip')
+  result = api_instance.service_post(
+      package,
+      name: '/etc/packages/my_packages/acs-aem-commons-content-test.zip',
+      install: true,
+      force: false,
+      strict: true
+  )
+  hash = XmlSimple.xml_in(result, ForceArray: false, KeyToSymbol: true)
+  puts result
+  puts "Service package upload: #{hash[:response][:data][:package][:name]}"
+  puts "Service package upload Status: #{hash[:response][:status]['code']}"
+rescue CrxPackageManager::ApiError => e
+  puts "Exception when calling DefaultApi->update: #{e}"
 end
